842e6f4946356e11ab9aa2134118924d8015ff7e
JDL Nolen <jdlnolen@gmail.com>
2014-06-11 13:16:36 -0500

    added install for laika

--- script.sh
+++ script.sh
@@ -7,6 +7,21 @@ sudo apt-get install python-software-properties python g++ make nodejs
 curl https://install.meteor.com | /bin/sh
 sudo -H npm install -g meteorite
 
+#install lakia and its dependecies for testing
+
+#install mongodb
+sudo apt-get -y install mongodb
+
+#install phantomjs
+cd tmp
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-x86_64.tar.bz2
+tar -xvf phantomjs-1.9.7-linux-x86_64.tar.bz2
+mv phantomjs-1.9.7-linux-x86_64 /opt
+export PATH=$PATH:/opt/phantomjs-1.9.7-linux-x86_64/bin
+
+#install laika
+sudo npm install -g laika
+
 #tests for successful install
 
 #assert function for testing
@@ -22,3 +37,6 @@ assert()
 assert node
 assert meteor
 assert mrt
+assert mongod
+assert phantomjs
+assert laika
